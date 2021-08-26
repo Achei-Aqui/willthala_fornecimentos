448c5b2eb9adcb7ca4b32f385e3ad77eafa5cdb2
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-08-26 00:46:01 -0300

    add MainHeader with index.js and style.css

--- front/src/components/MainHeader/index.js
+++ front/src/components/MainHeader/index.js
@@ -2,11 +2,11 @@ import './style.css';
 import '../../styles/tools/containers.css'
 const MainHeader = () => {
 	return /* html */ `
-		<div class="container">
-			<header>
-				<h1 class="main-title">Achei Aqui!</h1>
-			</header>
-		</div>
+
+		<header class="header-main-header">
+			<h1 class="main-title">Achei Aqui!</h1>
+		</header>
+
 	`;
 }
 
