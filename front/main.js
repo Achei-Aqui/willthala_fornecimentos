1537de36d3a2cfddfe1996dcca6be3dbcd844130
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-08-26 00:13:54 -0300

    add inserAdjacentHMTL

--- front/main.js
+++ front/main.js
@@ -1,10 +1,13 @@
 import './src/styles/settings/colors.css';
 import './src/styles/generics/reset.css';
-import './src/styles/elements/base.css';
+import './src/styles/elements/generic_base.css';
 
 import UserLogin from './src/objects/UserLogin';
 
 const $root = document.querySelector( '#root' );
 const $htmlLogin = UserLogin();
 
-$root.insertAdjacentHTML( 'beforeend', `${ $htmlLogin }` );
+$root.insertAdjacentHTML(
+	'beforeend',
+	`${ $htmlLogin }`
+);
