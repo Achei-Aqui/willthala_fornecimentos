7aedbcbb0d73355d19324fa73fea32a8bd63f7e9
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-08-26 00:50:08 -0300

    add Component FormLogin with index.js and style.css

--- front/src/components/FormLogin/index.js
+++ front/src/components/FormLogin/index.js
@@ -1,28 +1,31 @@
 import '../../styles/tools/containers.css';
+import '../../styles/elements/inputs_forms.css';
+import '../../styles/desktop/login_style.css'
 import './style.css';
-import '../../styles/desktop/style.css'
 
 const FormLogin = () => {
 	return /* html */ `
 		<div class="container">
-			<div class="form">
+			<div class="div-form-login">
+
 				<form action="POST" class="form-login">
+
 					<label for="cnpj-login">CNPJ:
 						<input id="cnpj-login" class="input -cnpj">
-
 					</label>
 
 					<br/>
 
 					<label for="password-login">Senha:
 						<input id="password-login" class="input -password">
-
 					</label>
 
 					<br/>
 
 				</form>
+
 			</div>
+
 		</div>
 	`;
 }
