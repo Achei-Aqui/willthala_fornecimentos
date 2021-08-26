d8c347fe7da164cc543c9dd11edba1922d4a5027
Laura Custodio <61993505+lauracarlotta@users.noreply.github.com>
2021-08-26 00:48:42 -0300

    add Component ButtonSend  with index.js

--- front/src/components/ButtonSend/index.js
+++ front/src/components/ButtonSend/index.js
@@ -1,13 +1,13 @@
-import './style.css';
 import '../../styles/tools/containers.css';
 import '../../styles/elements/link_register.css';
+import '../../styles/elements/button_send.css';
 
 const ButtonSend = () => {
 	return /* html */ `
 		<div class="container">
 
 			<button class="button-send">Entrar</button>
-			<a class="link-register" href="register.html">Cadastre-se</a>
+			<a class="link-register" href="src/html/register.html">Cadastre-se</a>
 
 		</div>
 
